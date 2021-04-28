0cb72e5aba7b66f963730daca8fb1fde7c573585
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-04-28 16:57:31 -0700

    Update Echo.cpp

--- Echo.cpp
+++ Echo.cpp
@@ -1,1 +1,23 @@
-
+#include <string>
+#include <iostream>
+#include "Echo.h"
+#include "Wav.h"
+#include "Processor.h"
+Echo::Echo() {
+}
+Echo::Echo(int newDelay){
+	Echo::setDelay(newDelay);
+}
+Echo::~Echo() {
+	std::cout<<"ha your mom gay"<<std::endl;
+}
+void Echo::processBuffer(unsigned char* buffer, int bufferSize){
+	int i;
+	int pain = Echo::delay;
+	for (i = pain; i <= bufferSize; i++){
+		buffer[i] = (buffer[i] + buffer[i - pain])/2;
+	}
+}
+void Echo::setDelay(int newDelay){
+	delay = newDelay;
+}
