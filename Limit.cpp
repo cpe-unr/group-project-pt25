cfb821c327116e1901af910ed979a196f7cf2d41
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-04-28 16:58:43 -0700

    Update Limit.cpp

--- Limit.cpp
+++ Limit.cpp
@@ -1,1 +1,23 @@
-
+#include <string>
+#include <iostream>
+#include "Limiter.h"
+#include "Wav.h"
+#include "Processor.h"
+Limiter::Limiter(){
+}
+Limiter::~Limiter(){
+	std::cout<<"ha your gun banned"<<std::endl;
+}
+void Limiter::processBuffer(unsigned char* buffer, int bufferSize){
+	for(int i = 0; i < bufferSize; i++) {
+		if(buffer[i] > 230 || buffer[i] < 25) {
+			if(buffer[i] > 230){
+				buffer[i] = 230;
+			}
+			else {
+				buffer[i] = 25;
+			}
+		}
+		
+	}
+}
