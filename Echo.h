e9982a69093c4f91f566b328a6f3fdf08e4c16ec
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-04-28 16:57:56 -0700

    Update Echo.h

--- Echo.h
+++ Echo.h
@@ -1,1 +1,17 @@
+#ifndef ECHO_H
+#define ECHO_H
 
+#include "Processor.h"
+#include <cstdint>
+#include <cmath>
+
+class Echo: public Processor {	   
+public:
+	int delay = 20000;
+	void processBuffer(unsigned char* buffer, int bufferSize);
+	Echo(int delay);
+	Echo();
+	virtual ~Echo();
+	void setDelay(int newDelay);
+};
+#endif //ECHO_H
