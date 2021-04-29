70e47d00d425df6025c8b227703ad1cc4f8a3912
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-04-28 17:00:01 -0700

    Update Noisegate.cpp

--- Noisegate.cpp
+++ Noisegate.cpp
@@ -1,1 +1,24 @@
+#include"Echo.h"
+#include"Processor.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "NoiseGate.h"
 
+void NoiseGate::processBuffer(unsigned char* buffer, int bufferSize)
+{
+	int yes = NoiseGate::index;
+	for(yes; yes <= bufferSize; yes++)
+	{
+		if(buffer[yes] <= 131 && buffer[yes] >= 125){
+			buffer[yes] = 128;
+		}
+	}
+
+}
+NoiseGate::NoiseGate()
+{
+}
+NoiseGate::~NoiseGate()
+{
+}
