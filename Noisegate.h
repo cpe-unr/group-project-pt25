f144e560638c4f04e2966296f765e8ab8934442d
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-04-29 15:26:03 -0700

    Update Noisegate.h

--- Noisegate.h
+++ Noisegate.h
@@ -1,1 +1,15 @@
+#ifndef NOISEGATE_H
+#define NOISEGATE_H
+#include"Processor.h"
 
+class NoiseGate : public Processor
+{
+public:
+	int index = 0;
+	void processBuffer(unsigned char* buffer, int bufferSize);
+	NoiseGate();
+	virtual ~NoiseGate();
+};
+
+#endif //NOISEGATE_H
+© 2021 GitHub, Inc.
