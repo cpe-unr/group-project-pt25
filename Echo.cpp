70b5adbcf257a298ae310858d141ceb5cf852173
asidhu-nevada <70528152+asidhu-nevada@users.noreply.github.com>
2021-05-01 16:22:40 -0700

    debugging and new getter function

--- Echo.cpp
+++ Echo.cpp
@@ -11,25 +11,25 @@ Echo::Echo(int newDelay){
 Echo::~Echo() {
 	
 }
-void Echo::processBuffer16BitStereo(int sizeLeft, int sizeRight, unsigned char* bufferLeft, unsigned char* bufferRight){ //Echo processor for 16 bit stereo
+void processBuffer16BitStereo(int sizeLeft, int sizeRight, unsigned char* bufferLeft, unsigned char* bufferRight){ //Echo processor for 16 bit stereo
 	for(int bufferIndex = 0; bufferIndex < sizeLeft-1; bufferIndex++){
 		if((bufferLeft[bufferIndex]) > 14090){
 		}
 		else{
-			bufferLeft[i] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-delay];
+			bufferLeft[bufferIndex] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-delay];
 		}
 	}
 
 	for(int bufferIndex = 0; bufferIndex < sizeRight-1; bufferIndex++){
-		if((bufferRight[i]) > 14090){
+		if((bufferRight[bufferIndex]) > 14090){
 		}
 		else{
-			bufferRight[i] = bufferRight[i] + bufferRight[i-delay];
+			bufferRight[bufferIndex] = bufferRight[bufferIndex] + bufferRight[bufferIndex-delay];
 		}
 	}
 }
-void Echo::processBuffer16Bitmono(unsigned char* buffer, int bufferSize){ //Echo processor for 16 bit mono
-	for (int i = 0; i <= bufferSize; i++){
+void processBuffer16Bitmono(unsigned char* buffer, int bufferSize){ //Echo processor for 16 bit mono
+	for (int index = 0; index <= bufferSize; index++){
 		if((buffer[i]) > 14090){
 		}
 		else{
@@ -37,25 +37,25 @@ void Echo::processBuffer16Bitmono(unsigned char* buffer, int bufferSize){ //Echo
 		}
 	}
 }
-void Echo::process8BitStereo(int sizeLeft, int sizeRight, unsigned char* bufferLeft, unsigned char* bufferRight){ //Echo processor for 8 Bit stereo
+void process8BitStereo(int sizeLeft, int sizeRight, unsigned char* bufferLeft, unsigned char* bufferRight){ //Echo processor for 8 Bit stereo
 
 	for(int bufferIndex = 0; bufferIndex < sizeLeft-1; bufferIndex++){
 		if((bufferLeft[bufferIndex]) > 110){
 		}
 		else{
-			bufferLeft[i] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-delay];
+			bufferLeft[bufferIndex] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-delay];
 		}
 	}
 
 	for(int bufferIndex = 0; bufferIndex < sizeRight-1; bufferIndex++){
-		if((bufferRight[i]) > 110){
+		if((bufferRight[bufferIndex]) > 110){
 		}
 		else{
-			bufferRight[i] = bufferRight[i] + bufferRight[i-delay];
+			bufferRight[bufferIndex] = bufferRight[i] + bufferRight[i-delay];
 		}
 	}
 }
-void Echo::processBuffer8bitMono(unsigned char* buffer, int bufferSize){ //Echo processor for 8 bit mono
+void processBuffer8bitMono(unsigned char* buffer, int bufferSize){ //Echo processor for 8 bit mono
 	for(int i = 0; i < bufferSize-1; i++){
 		if((buffer[i]) > 110){
 		}
@@ -66,4 +66,7 @@ void Echo::processBuffer8bitMono(unsigned char* buffer, int bufferSize){ //Echo 
 }
 void Echo::setDelay(int newDelay){
 	delay = newDelay;
+}
+int Echo::getDelay(){
+	return delay;
 }
