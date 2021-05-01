670a1e54346acc0269510fec864f98242eb41f54
asidhu-nevada <asidhu@nevada.unr.edu>
2021-05-01 16:56:05 -0700

    need to find out how to eliminate scope error

--- Echo.cpp
+++ Echo.cpp
@@ -12,37 +12,39 @@ Echo::~Echo() {
 	
 }
 void processBuffer16BitStereo(int sizeLeft, int sizeRight, unsigned char* bufferLeft, unsigned char* bufferRight){ //Echo processor for 16 bit stereo
+	int cDelay = delay;
 	for(int bufferIndex = 0; bufferIndex < sizeLeft-1; bufferIndex++){
 		if((bufferLeft[bufferIndex]) > 14090){
 		}
 		else{
-			bufferLeft[bufferIndex] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-Echo::delay];
+			bufferLeft[bufferIndex] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-cDelay];
 		}
 	}
 	for(int bufferIndex = 0; bufferIndex < sizeRight-1; bufferIndex++){
 		if((bufferRight[bufferIndex]) > 14090){
 		}
 		else{
-			bufferRight[bufferIndex] = bufferRight[bufferIndex] + bufferRight[bufferIndex-Echo::delay];
+			bufferRight[bufferIndex] = bufferRight[bufferIndex] + bufferRight[bufferIndex-cDelay];
 		}
 	}
 }
 void processBuffer16Bitmono(unsigned char* buffer, int bufferSize){ //Echo processor for 16 bit mono
+	int cDelay = delay;
 	for (int i = 0; i <= bufferSize; i++){
 		if((buffer[i]) > 14090){
 		}
 		else{
-			buffer[i] = buffer[i] + buffer[i-Echo::delay];
+			buffer[i] = buffer[i] + buffer[i-cDelay];
 		}
 	}
 }
 void process8BitStereo(int sizeLeft, int sizeRight, unsigned char* bufferLeft, unsigned char* bufferRight){ //Echo processor for 8 Bit stereo
-
+	int cDelay = delay;
 	for(int bufferIndex = 0; bufferIndex < sizeLeft-1; bufferIndex++){
 		if((bufferLeft[bufferIndex]) > 110){
 		}
 		else{
-			bufferLeft[bufferIndex] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-Echo::delay];
+			bufferLeft[bufferIndex] = bufferLeft[bufferIndex] + bufferLeft[bufferIndex-cDelay];
 		}
 	}
 
@@ -50,16 +52,17 @@ void process8BitStereo(int sizeLeft, int sizeRight, unsigned char* bufferLeft, u
 		if((bufferRight[bufferIndex]) > 110){
 		}
 		else{
-			bufferRight[bufferIndex] = bufferRight[bufferIndex] + bufferRight[bufferIndex-Echo::delay];
+			bufferRight[bufferIndex] = bufferRight[bufferIndex] + bufferRight[bufferIndex-cDelay];
 		}
 	}
 }
 void processBuffer8bitMono(unsigned char* buffer, int bufferSize){ //Echo processor for 8 bit mono
+	int cDelay = delay;
 	for(int i = 0; i < bufferSize-1; i++){
 		if((buffer[i]) > 110){
 		}
 		else{
-			buffer[i] = buffer[i] + buffer[i-Echo::delay];
+			buffer[i] = buffer[i] + buffer[i-cDelay];
 		}
 	}
 }
